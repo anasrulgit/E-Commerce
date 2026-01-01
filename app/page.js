5e8ec669974e73be887bc0ef86a807b58a3b6835
unknown <putraanasrul3@gmail.com>
2026-01-01 14:02:21 +0700

    chore: initial setup with Docker, Prisma, Next.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>E-Commerce Fullstack Project</h1>
+      <p>Database: PostgreSQL (Docker)</p>
+      <p>Frontend: Next.js 14</p>
+      <p>Backend: Next.js API Routes</p>
+    </main>
+  );
+}
